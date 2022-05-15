34577d277f29856465185eb00c3118238e0810f4
syumai <syumai@gmail.com>
2022-05-15 10:21:40 +0900

    use std base64

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
+import { encode } from "https://deno.land/std@0.139.0/encoding/base64.ts";
 const bytes = await Deno.readAll(Deno.stdin);
 const dec = new TextDecoder("UTF-8");
-const encoded = btoa(dec.decode(bytes));
+const encoded = encode(dec.decode(bytes));
 console.log(`export default "${encoded}";`);
